0708441c6e2c5b850f19a1e41ea913a533c7614f
nuuuwan <nuuuwan@gmail.com>
2022-11-09 12:25:19 +0530

    feat: workflow on multiple PDF files

--- workflows/parse_and_expand.py
+++ workflows/parse_and_expand.py
@@ -2,5 +2,5 @@ from census_lk_pdf_parser.expand import expand
 from census_lk_pdf_parser.parse import parse
 
 if __name__ == '__main__':
-    pdf_file = 'data/education.pdf'
-    expand(parse(pdf_file))
+    for pdf_file in ['data/education.pdf']:
+        expand(parse(pdf_file))
