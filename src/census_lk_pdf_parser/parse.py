6a16db14ca91c09672deffc18e54c54f32b1ca3e
nuuuwan <nuuuwan@gmail.com>
2022-11-10 13:54:00 +0530

    feat: made parse generic

--- src/census_lk_pdf_parser/parse.py
+++ src/census_lk_pdf_parser/parse.py
@@ -1,12 +1,64 @@
-import os
 
 import tabula
 from utils import TSVFile, logx
 
 log = logx.get_logger('census_lk_pdf_parser.parse')
+DELIM = '___'
 
 
-def parse(pdf_file, parse_row):
+def parse_int(x):
+    if str(x) == 'nan':
+        return 0
+    x = str(x).replace(',', '')
+    return (int)(x)
+
+
+def parse_gnd_num(x):
+    if str(x) == 'nan':
+        return '-'
+    return str(x)
+
+
+def extract_cells(row):
+    row[0] = str(row[0]).replace(' ', '_')
+    row[1] = str(row[1]).replace(' ', '_')
+    str_cell_list = list(
+        map(
+            lambda x: str(x).replace(' ', DELIM),
+            row,
+        )
+    )
+    return DELIM.join(str_cell_list).split(DELIM)
+
+def validate(row, cells, d, field_name_list):
+    total = d['total']
+    total2 = sum(list(map(
+        lambda field_name: d[field_name],
+        field_name_list[1:],
+    )))
+    assert total == total2, row
+
+
+def parse_row(row, has_gnd_num, field_name_list):
+    cells = extract_cells(row)
+    region_name = cells[0].replace('_', ' ')
+
+    d = {}
+    d['region_name'] = region_name
+    offset = 0
+    if has_gnd_num:
+        d['gnd_num'] = parse_gnd_num(cells[1].replace('_', ''))
+        offset = 1
+
+    for i, field_name in enumerate(field_name_list):
+        d[field_name] = parse_int(cells[i + offset + 1])
+
+    validate(row, cells, d, field_name_list)
+
+    return d
+
+
+def parse(pdf_file, has_gnd_num, field_names):
     tsv_file = pdf_file[:-4] + '.tsv'
     log.warn(f'{tsv_file} already exits')
 
@@ -14,16 +66,24 @@ def parse(pdf_file, parse_row):
     names = [str(i) for i in range(8)]
     df = tabula.read_pdf(
         pdf_file,
-        pages="all",
+        pages="1-100",
         multiple_tables=False,
         pandas_options=dict(names=names),
     )[0]
 
+    log.info(f'Parsing {pdf_file} rows...')
     data_list = []
     N_HEADER = 5
-    for row in df.values.tolist()[N_HEADER:]:
-        data = parse_row(row)
+    rows = df.values.tolist()[N_HEADER:]
+    for i, row in enumerate(rows):
+        try:
+            data = parse_row(row, has_gnd_num, field_names)
+        except:
+            log.error(row)
         data_list.append(data)
+
+        if i % 1_000 == 0:
+            log.debug(data)
 
     tsv_file = pdf_file[:-4] + '.tsv'
     TSVFile(tsv_file).write(data_list)
--- workflows/parse_and_expand.py
+++ workflows/parse_and_expand.py
@@ -1,9 +1,37 @@
-from census_lk_pdf_parser import (expand, parse, parse_economic_activity,
-                                  parse_education)
+from census_lk_pdf_parser import expand, parse
+
+CONFIG_LIST = [
+    dict(
+        pdf_file='data/education.pdf',
+        has_gnd_num=False,
+        field_name_list=[
+            'total',
+            'primary',
+            'secondary',
+            'gce_ordinary_level',
+            'gce_advanced_level',
+            'degree_and_above',
+            'no_schooling',
+        ],
+    ),
+    dict(
+        pdf_file='data/economic-activity.pdf',
+        has_gnd_num=True,
+        field_name_list=[
+            'total',
+            'employed',
+            'unemployed',
+            'economically_not_active',
+        ],
+    ),
+]
 
 if __name__ == '__main__':
-    for pdf_file, parse_row in [
-        ['data/economic-activity.pdf', parse_economic_activity.parse_row],
-        ['data/education.pdf', parse_education.parse_row],
-    ]:
-        expand.expand(parse.parse(pdf_file, parse_row))
+    for config in CONFIG_LIST:
+        expand.expand(
+            parse.parse(
+                config['pdf_file'],
+                config['has_gnd_num'],
+                config['field_name_list'],
+            )
+        )
