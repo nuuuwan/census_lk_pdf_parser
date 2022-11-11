8a7c79515f03a2993d2b85a8d58507562f1e48f2
nuuuwan <nuuuwan@gmail.com>
2022-11-11 10:18:24 +0530

    fix: case 3a BUG

--- src/census_lk_pdf_parser/regionx.py
+++ src/census_lk_pdf_parser/regionx.py
@@ -1,7 +1,6 @@
 from gig import ent_types, ents
 from gig.ent_types import ENTITY_TYPE
 from utils import logx
-from utils.cache import cache
 
 log = logx.get_logger('census_lk_pdf_parser.regionx')
 
@@ -12,6 +11,7 @@ def get_district_id(region_id):
 
 def get_dsd_id(region_id):
     return region_id[:7]
+
 
 def get_next_region_type(previous_region_type):
     if previous_region_type == ENTITY_TYPE.COUNTRY:
@@ -26,65 +26,97 @@ def get_next_region_type(previous_region_type):
     return None
 
 
+def get_unvisited_region_id(candidate_regions, visited_region_id_list):
+    for candidate_region in candidate_regions:
+        candidate_region_id = candidate_region['id']
+        if candidate_region_id in visited_region_id_list:
+            continue
+        return candidate_region_id
+    return None
 
-@cache('get_region_id', 86400 * 1000)
+
 def get_region_id_inner(
     data, previous_known_region_id, visited_region_id_list, min_fuzz_ratio
 ):
     region_name = data['region_name']
+
+    # CASE 1: region_type == 'country'
     if region_name == 'Sri Lanka':
         return 'LK'
 
-    region_id = None
-    candidate_region_type = None
     if previous_known_region_id:
         previous_region_type = ent_types.get_entity_type(
             previous_known_region_id
         )
+
+    if previous_region_type:
         candidate_region_type = get_next_region_type(previous_region_type)
 
+    # CASE 2: previous_region_type in ['country', 'district', 'dsd']
+    if candidate_region_type:
+        candidate_regions = ents.get_entities_by_name_fuzzy(
+            region_name,
+            filter_entity_type=candidate_region_type,
+            filter_parent_id=previous_known_region_id,
+            limit=5,
+            min_fuzz_ratio=min_fuzz_ratio,
+        )
+        return get_unvisited_region_id(
+            candidate_regions, visited_region_id_list
+        )
+
+    # CASE 3: previous_region_type = 'gnd'
+    # CASE 3a: Next row is a gnd
+    dsd_id = get_dsd_id(previous_known_region_id)
     candidate_regions = ents.get_entities_by_name_fuzzy(
         region_name,
-        filter_entity_type=candidate_region_type,
+        filter_entity_type='gnd',
+        filter_parent_id=dsd_id,
+        limit=5,
+        min_fuzz_ratio=min_fuzz_ratio,
+    )
+    region_id_case3a = get_unvisited_region_id(
+        candidate_regions, visited_region_id_list
+    )
+    if region_id_case3a:
+        return region_id_case3a
+
+    # CASE 3b: Next row is NOT a gnd
+    candidate_regions = ents.get_entities_by_name_fuzzy(
+        region_name,
+        filter_entity_type=None,
         filter_parent_id=None,
         limit=5,
         min_fuzz_ratio=min_fuzz_ratio,
     )
 
-    if candidate_region_type:
-        if candidate_regions:
-            return candidate_regions[0]['id']
-    else:
-        for candidate_region in candidate_regions:
-            candidate_region_id = candidate_region['id']
-            if candidate_region_id in visited_region_id_list:
-                continue
+    for candidate_region in candidate_regions:
+        candidate_region_id = candidate_region['id']
+        if candidate_region_id in visited_region_id_list:
+            continue
+        candidate_region_type = ent_types.get_entity_type(candidate_region_id)
 
-            candidate_region_type = ent_types.get_entity_type(
-                candidate_region_id
-            )
+        if candidate_region_type == ENTITY_TYPE.DISTRICT:
+            return candidate_region_id
 
-            if candidate_region_type == ENTITY_TYPE.DISTRICT:
+        elif candidate_region_type == ENTITY_TYPE.DSD:
+            if all(
+                [
+                    get_district_id(candidate_region_id)
+                    == get_district_id(previous_known_region_id),
+                    get_dsd_id(candidate_region_id)
+                    != get_dsd_id(previous_known_region_id),
+                ]
+            ):
                 return candidate_region_id
 
-            elif candidate_region_type == ENTITY_TYPE.DSD:
-                if all(
-                    [
-                        get_district_id(candidate_region_id)
-                        == get_district_id(previous_known_region_id),
-                        get_dsd_id(candidate_region_id)
-                        != get_dsd_id(previous_known_region_id),
-                    ]
-                ):
-                    return candidate_region_id
-
-            # candidate_region_type == ENTITY_TYPE.GND
-            else:
-                if get_dsd_id(candidate_region_id) == get_dsd_id(
-                    previous_known_region_id
-                ):
-                    return candidate_region_id
-    return region_id
+        # candidate_region_type == ENTITY_TYPE.GND
+        else:
+            if get_dsd_id(candidate_region_id) == get_dsd_id(
+                previous_known_region_id
+            ):
+                return candidate_region_id
+    return None
 
 
 def get_region_id(data, previous_known_region_id, visited_region_id_list):
@@ -102,12 +134,20 @@ def get_region_id(data, previous_known_region_id, visited_region_id_list):
 
 if __name__ == '__main__':
     candidate_regions = ents.get_entities_by_name_fuzzy(
-        'Meerigama',
-        filter_entity_type=None,
-        filter_parent_id=None,
-        limit=5,
-        min_fuzz_ratio=80,
+        'Medagama III',
+        filter_entity_type='gnd',
+        filter_parent_id='LK-1224',
+        limit=10,
+        min_fuzz_ratio=60,
     )
     for candidate_region in candidate_regions:
         print()
         print(candidate_region)
+    #
+    # region_id = get_region_id_inner(
+    #     dict(region_name='Kolonnawa'),
+    #     previous_known_region_id='LK-1103175',
+    #     visited_region_id_list=[],
+    #     min_fuzz_ratio=90,
+    # )
+    # print(region_id)
